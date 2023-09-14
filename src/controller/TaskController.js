c2be5e8074a3bd6cb56a12d82e6d3835cd7953a1
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-09-14 20:59:27 +0500

    add task

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -6,7 +6,7 @@ const moment = require("moment");
 const create_task = async (req, res) => {
   try {
     const adminId = req.id;
-    const { title, description, amount } = req.body;
+    const { title, description, amount,completion_date } = req.body;
     if (!title) {
       return res.status(404).send({
         status: 0,
@@ -22,6 +22,11 @@ const create_task = async (req, res) => {
         status: 0,
         message: "please enter amount",
       });
+    }else if(!completion_date){
+        return res.status(404).send({
+            status: 0,
+            message: "please enter a completion date",
+          });
     }
     const admin = await User.findOne({ _id: adminId, role: "admin" });
     if (!admin) {
@@ -35,7 +40,7 @@ const create_task = async (req, res) => {
       title,
       description,
       amount,
-      create_date:moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a'), 
+      create_date: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
       status: "todo",
       createdBy_id: adminId,
     });
@@ -59,23 +64,23 @@ const assign_task = async (req, res) => {
   try {
     const adminId = req.id;
     const userId = req.query.user_id;
-    const task_id=req.query.task_id
-    if(!task_id){
-        return res.status(404).send({
-            status: 0,
-            message: "please enter user id",
-          });
-    }else if (!mongoose.isValidObjectId(userId)) {
+    const task_id = req.query.task_id;
+    if (!task_id) {
+      return res.status(404).send({
+        status: 0,
+        message: "please enter user id",
+      });
+    } else if (!mongoose.isValidObjectId(userId)) {
       console.error("Error", "not valid user".red);
       return res.status(404).send({
         status: 0,
         message: "not valid user",
       });
-    }else if(!mongoose.isValidObjectId(task_id)){
-        return res.status(404).send({
-            status: 0,
-            message: "not a valid task",
-          });
+    } else if (!mongoose.isValidObjectId(task_id)) {
+      return res.status(404).send({
+        status: 0,
+        message: "not a valid task",
+      });
     }
     const taskUser = await User.findOne({ _id: userId, role: "user" });
     if (!taskUser) {
@@ -91,27 +96,39 @@ const assign_task = async (req, res) => {
         message: "you are not Admin",
       });
     }
-    const task=await Task.findOne({_id:task_id})
-    if(!task){
-        return res.status(404).send({
-            status: 0,
-            message: "No such task exists",
-          });
+    const task = await Task.findOne({ _id: task_id });
+    if (!task) {
+      return res.status(404).send({
+        status: 0,
+        message: "No such task exists",
+      });
     }
-    const task_update=await Task.findOneAndUpdate(
-        {_id:task?._id.toString()},
-        {
-            freeLancer_id:userId,
-            status:"assigned",
-            assign_date:moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a'),
-        },
-        {new:true}
-    )
+    const task_update = await Task.findOneAndUpdate(
+      { _id: task?._id.toString() },
+      {
+        freeLancer_id: userId,
+        status: "assigned",
+        assign_date: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
+      },
+      { new: true }
+    );
     res.status(200).send({
-        status:0,
-        message:"successfully assigned task",
-        task_update
-    })
+      status: 0,
+      message: "successfully assigned task",
+      task_update,
+    });
+  } catch (err) {
+    console.log("Error", err.message.red);
+    res.status(500).send({
+      status: 0,
+      message: "Something went wrong",
+      error: err.message,
+    });
+  }
+};
+
+const accept_task = async (req, res) => {
+  try {
   } catch (err) {
     console.log("Error", err.message.red);
     res.status(500).send({
@@ -124,4 +141,5 @@ const assign_task = async (req, res) => {
 module.exports = {
   create_task,
   assign_task,
+  accept_task,
 };
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -2,11 +2,13 @@ const express=require("express");
 const user_token_auth = require("../middleware/Auth");
 const file=require("../middleware/Multer");
 const { create_task,
-    assign_task } = require("../controller/TaskController");
+    assign_task,
+    accept_task } = require("../controller/TaskController");
 const router=express.Router()
 
 router.post("/create_task",file.user,user_token_auth,create_task);
 router.put("/assign_task/",file.user,user_token_auth,assign_task);
+router.put("/accept_task/",file.user,user_token_auth,accept_task);
 
 
 module.exports=router

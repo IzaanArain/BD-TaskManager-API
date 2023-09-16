e686f65b14af332f35dbca0f0e2a6ab4bf7f1599
IzaanArain <izaansaquib@gmail.com>
2023-09-16 11:22:45 +0500

    add task

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -6,7 +6,14 @@ const moment = require("moment");
 const create_task = async (req, res) => {
   try {
     const adminId = req.id;
-    const { title, description, amount,completion_date } = req.body;
+    const { title, description, amount, completion_date } = req.body;
+    const admin = await User.findOne({ _id: adminId, role: "admin" });
+    if (!admin) {
+      return res.status(404).send({
+        status: 0,
+        message: "you are not Admin",
+      });
+    }
     if (!title) {
       return res.status(404).send({
         status: 0,
@@ -22,34 +29,52 @@ const create_task = async (req, res) => {
         status: 0,
         message: "please enter amount",
       });
-    }else if(!completion_date){
-        return res.status(404).send({
-            status: 0,
-            message: "please enter a completion date",
-          });
-    }
-    const admin = await User.findOne({ _id: adminId, role: "admin" });
-    if (!admin) {
+    } else if (!completion_date) {
       return res.status(404).send({
         status: 0,
-        message: "you are not Admin",
+        message: "please enter a completion date",
+      });
+    }
+    const com_date = completion_date.split("T")[0];
+    if (
+      !com_date.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)
+    ) {
+      return res.status(404).send({
+        status: 0,
+        message: "date format must match YYYY-MM-DD",
       });
     }
 
+    const date1=moment(Date.now())
+    const date2=moment(com_date)
+    if(date2.isBefore(date1)){
+      return res.status(404).send({
+        status: 0,
+        message: "completion date can not before creation date",
+      });
+    }
+    const future_date=date1.add(1,"M");
+    if(date2.isAfter(future_date)){
+      return res.status(404).send({
+        status: 0,
+        message: "completion date can not be beyond 1 month",
+      });
+    }
     const task = await Task.create({
       title,
       description,
       amount,
       create_date: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
+      completion_date:date2.format("MMMM Do YYYY, h:mm:ss a"),
       status: "todo",
       createdBy_id: adminId,
     });
-
     res.status(200).send({
       status: 1,
       message: "Successfully created task",
-      task,
+      task
     });
+
   } catch (err) {
     console.error("Error", err.message.red);
     res.status(500).send({
